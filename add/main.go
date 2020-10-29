105cfb53cf39255bcdc59455d6c6e11417a6b4d7
Tharun <rajendrantharun@live.com>
2020-10-29 12:20:55 +0530

    Initial Commit of kuma grpc support

---
+++ add/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"github.com/shuza/kubernetes-go-grpc/pd"
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+	"log"
+	"net"
+)
+
+/**
+ * :=  created by:  Shuza
+ * :=  create date:  29-Jan-2019
+ * :=  (C) CopyRight Shuza
+ * :=  www.shuza.ninja
+ * :=  shuza.sa@gmail.com
+ * :=  Fun  :  Coffee  :  Code
+ **/
+
+type server struct{}
+
+func main() {
+	lis, err := net.Listen("tcp", ":3000")
+	if err != nil {
+		log.Fatalf("Failed to listen:  %v", err)
+	}
+
+	s := grpc.NewServer()
+	pb.RegisterAddServiceServer(s, &server{})
+	reflection.Register(s)
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("Failed to serve: %v", err)
+	}
+}
+
+func (s *server) Compute(cxt context.Context, r *pb.AddRequest) (*pb.AddResponse, error) {
+	result := &pb.AddResponse{}
+	result.Result = r.A + r.B
+
+	logMessage := fmt.Sprintf("A: %d   B: %d     sum: %d", r.A, r.B, result.Result)
+	log.Println(logMessage)
+
+	return result, nil
+}
